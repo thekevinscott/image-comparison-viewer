d5f0901e2878c6bdbccac37fbeba0bd47b9638a2
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-08-14 16:21:35 -0400

    Update types

--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -1,4 +1,4 @@
 import "./components/viewer";
 import "./components/dragger";
 export { ImageComparisonViewer } from './components/viewer';
-export { Dragger } from './components/dragger';
+export { DraggerChangeEvent, ImageComparisonViewerDraggerHandle } from './components/dragger';
